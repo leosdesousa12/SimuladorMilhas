709f7a45a812bef4a0db199f919d4c450f089835
leosdesousa12 <leosdesousa@academico.ifpb.edu.br>
2019-11-29 13:35:59 -0300

    Finalizando terceira tela, iniciar parte de refatoramento e testes

--- SimuladorMilhas/src/app/app.component.ts
+++ SimuladorMilhas/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {cartoes} from '../app/cartoes'
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ɵConsole} from '@angular/core';
 import {Observable} from 'rxjs';
 import {debounceTime,  map} from 'rxjs/operators';
 import { FormControl ,FormGroup} from '@angular/forms';
 import { Pipe } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 
 
 
@@ -17,28 +18,43 @@ export class AppComponent implements OnInit {
   title = 'Calculadora De Milhas';
   public model: any;
 
+  private REST_API_SERVER = 
+  "https://economia.awesomeapi.com.br/jsonp/USDT-BRL/";
+  
+  constructor(private httpClient: HttpClient) { 
+
+  }
+
   menu= {op1: false,op2:false,op3:false};
   cont = 0;
+  dolar = 0;
   primeiraTela = {pontos:0 , meses:0, custo:0.00};
 
   segundaTela = {pontos:1 , meses:1, custo:0.00};
+
+  terceiraTela = {pontos:1 , meses:1, custo:0.00};
 
   myFunction(param: any){
     if(this.primeiraTela.pontos == 0 || this.primeiraTela.meses == 0){
       this.primeiraTela.custo = 0.0;
   }else{
-      this.primeiraTela.custo = (((this.primeiraTela.pontos/this.primeiraTela.meses)/this.model.valPonto) * 4.00);
+      this.primeiraTela.custo = (((this.primeiraTela.pontos/this.primeiraTela.meses)/this.model.valPonto) * this.dolar);
   }
   }
   myFunction_2(param: any){
-    console.log(param);
     if(this.segundaTela.pontos == 0 || this.segundaTela.custo == 0){
       this.segundaTela.meses =  0;
-      console.log("erado");
 
     }else{
-      this.segundaTela.meses =  (this.segundaTela.pontos/((this.segundaTela.custo/4.00) * this.model.valPonto));
-      console.log("meses");
+      this.segundaTela.meses =  (this.segundaTela.pontos/((this.segundaTela.custo/ this.dolar) * this.model.valPonto));
+  }
+  }
+
+  myFunction_3(param: any){
+    if(this.terceiraTela.custo == 0 || this.terceiraTela.meses == 0){
+        this.terceiraTela.pontos =  0;
+    }else{
+       this.terceiraTela.pontos = ((this.terceiraTela.custo/ this.dolar) * this.model.valPonto) * this.terceiraTela.meses;
   }
   }
 
@@ -53,7 +69,9 @@ export class AppComponent implements OnInit {
     
   ngOnInit(){
     this.model = "";
-    }
+    this.dolar = 0;
+    this.getDolar();
+  }
 
   opcoesBotao(op1:boolean,op2:boolean,op3:boolean){
       this.menu.op1 = op1;
@@ -61,12 +79,20 @@ export class AppComponent implements OnInit {
       this.menu.op3 = op3;
   }
 
+  getDolar() {
+    console.log("Dolar");
+    this.sendGetRequest().subscribe((data: any[])=>{
+      //console.log(data[0].ask);
+      this.dolar = data[0].ask;
 
-  menu_1 = (text$: Observable<string>) =>
-  text$.pipe(
-    debounceTime(200),
-    map(term => term === '' ? []
-      : console.log("")
-  ))
+    })  
+  }
+
+   sendGetRequest(){
+    return this.httpClient.get(this.REST_API_SERVER);
+  }
+
+
+
 
 }
--- SimuladorMilhas/src/app/app.module.ts
+++ SimuladorMilhas/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  //<<<< import it here
 import { ReactiveFormsModule } from '@angular/forms';  //<<<< import it here
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +26,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
